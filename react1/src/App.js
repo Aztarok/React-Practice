5a68dd59a475fe1b834b0031a4ee67bda58989da
Aztarok <aztarok2994@gmail.com>
2023-04-03 14:42:21 -0400

    Basic Todo app in the react1 folder done

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Task } from './Task';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleTask = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: tasks.length === 0 ? 1 : tasks[tasks.length-1].id + 1,
+      taskName: newTask,
+      completed: false,
+    }
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  const completeTask = (id) => {
+    setTasks(tasks.map((task) => {
+      if(task.id === id) {
+        return {...task, completed: true};
+      } else {
+        return task;
+      }
+    }))
+  }
+
+  return (<div className="App">
+      <div className="addTask">
+        <input onChange={handleTask} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {tasks.map((task) => {
+          return <Task
+            taskName={task.taskName}
+            id={task.id}
+            completed={task.completed}
+            deleteTask={deleteTask} 
+            completeTask={completeTask}
+            />
+        })}
+      </div>
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
